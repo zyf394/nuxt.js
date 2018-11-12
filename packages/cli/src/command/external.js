0589835e89b715c45192c5b246111a93870a4a7a
Jonas Galvez <jonasgalvez@gmail.com>
2018-11-12 01:05:49 -0200

    wip: new command subclasses (2)

--- packages/cli/src/command/external.js
+++ packages/cli/src/command/external.js
@@ -1,5 +1,17 @@
 import NuxtCommand from './index'
 
-export class ExternalNuxtCommand extends NuxtCommand () {
-
+export class ExternalNuxtCommand extends NuxtCommand {
+  static resolveExternal(cmd) {
+    const resolvers = [
+      () => require.resolve(`@nuxt/${cmd}`),
+      () => require.resolve(`@nuxtjs/${cmd}`),
+      () => require.resolve(`${cmd}`)
+    ]  
+    do {
+      const command = resolvers.shift()()
+      if (command) {
+        return command
+      }
+    } while (resolvers.length)
+  }
 }
--- packages/cli/src/command/local.js
+++ packages/cli/src/command/local.js
@@ -1,12 +1,24 @@
-import NuxtCommand from './index'
+import { resolve, join, parse } from 'path'
+import { readdirSync, existsSync } from 'fs'
+import { requireModule } from '../utils'
+import NuxtCommand from './command'
 
-export class LocalNuxtCommand extends NuxtCommand () {
+export class LocalNuxtCommand extends NuxtCommand {
 
   static existsLocal(cmd) {
     const cmdsRoot = resolve('.', 'commands')
     if (existsSync(cmdsRoot)) {
       return this.filterCommands(cmdsRoot).includes(`${cmd}.js`)
     }
+  }
+
+  static loadLocal(cmd) {
+    const cmdsRoot = resolve('.', 'commands')
+    const file = filterCommands(cmdsRoot).find((c) => {
+      return parse(c).name === cmd
+    })
+    const command = requireModule(join(cmdsRoot, file))
+    return NuxtCommand.from(command.default)
   }
 
 }
